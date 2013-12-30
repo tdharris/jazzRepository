1ef2898b5d46f23e49db36e1c3f63f37b4321a4a
tdharris <tylerdavidharris@gmail.com>
2013-12-30 11:56:06 -0700

    added css spinning-loader

--- public/js/snippetsLibrary.js
+++ public/js/snippetsLibrary.js
@@ -3,14 +3,16 @@ var myApp = angular.module('snippetsLib', []);
 myApp.factory('snippetsFactory', function($http) {
 	return {
       getSnippetsAsync: function(callback) {
-          $http.get('snippets').success(callback);
+      	$( "div.loader" ).last().addClass( "loading-overlay" );
+		$http.get('snippets').success(callback);
       }
   };
 });
 
 myApp.controller('SnippetsCtrl', function($scope, snippetsFactory) {
   snippetsFactory.getSnippetsAsync(function(results) {
-      console.log('SnippetsCtrl async returned value: ' + results);
-      $scope.Snippets = results;
+  	$( "div.loader" ).last().removeClass( "loading-overlay" );
+	console.log('SnippetsCtrl async returned value: ' + results);
+	$scope.Snippets = results;
   });
 });
